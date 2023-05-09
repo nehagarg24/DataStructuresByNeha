23a284866b44ee26e7394a73fc30beef52f2523b
Neha1234garg <neha.garg@appdirect.com>
2023-05-09 14:18:24 +0530

    testing

---
+++ emp-login-crud/src/test/java/com/emplogincrud/EmpLoginCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emplogincrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpLoginCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
